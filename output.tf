2632c4f440fbfcecd48d2372c7fe88a0ec67d989
Igor Kotov <igor.kotof@gmail.com>
2021-03-22 23:47:19 +0300

    fix outputs

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "cmd" {
-  value = merge({},{
+  value = {
     up     = "ssh -M -S bastion.sock -fNT ubuntu@${element(aws_instance.this.*.id, 0)} "
     down   = "ssh -S bastion.sock -O exit ubuntu@${element(aws_instance.this.*.id, 0)} "
     status = "ssh -S bastion.sock -O check ubuntu@${element(aws_instance.this.*.id, 0)}"
-  })
+  }
 }
 
 output "instance_id" {
@@ -11,7 +11,7 @@ output "instance_id" {
 }
 
 output "ssh_config" {
-  value = join("", local.ssh_config)
+  value = local.ssh_config
 }
 output "security_group" {
   value = join("", aws_security_group.this.id)
