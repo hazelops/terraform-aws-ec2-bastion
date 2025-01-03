5ba7a7e0a97c4330aa496bd4b470879ff3742861
Dmitry Kireev <dmitry@hazelops.com>
2025-01-03 08:58:13 +0000

    Update instance arch regex to match all graviton instances

--- locals.tf
+++ locals.tf
@@ -1,8 +1,3 @@
 locals {
-  instance_arch = contains([
-    "t4g",
-    "m6g",
-    "c6g",
-    "r6g"
-  ], substr(var.instance_type, 0, 3)) ? "arm64" : "x86_64"
+  instance_arch = can(regex("\\w\\dg\\..*", var.instance_type)) ? "arm64" : "x86_64"
 }
